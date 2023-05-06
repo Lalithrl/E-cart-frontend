307eae50a137289c2345f7a4cce5c15905229ae6
Lalithrl <lalithrl5432@gmail.com>
2023-05-06 19:46:54 +0530

    first commit

---
+++ src/components/back/Data/Data.js
@@ -0,0 +1,60 @@
+const data = {
+    productItems:[
+        {
+            id : "1",
+            Name : "Boat Headphone",
+            price : 1999,
+            image : "./pics/1.webp"
+        },
+        {
+            id : "2",
+            Name : "Noise Headphone",
+            price : 1899,
+            image : "./pics/2.webp"
+        },
+        {
+            id : "3",
+            Name : "Skullcandy Headphone",
+            price : 2099,
+            image : "./pics/3.webp"
+        },
+        {
+            id : "4",
+            Name : "Realme Buds",
+            price : 1799,
+            image : "./pics/4.png"
+        },
+        {
+            id : "5",
+            Name : "Apple Airpods",
+            price : 2199,
+            image : "./pics/5.jpg"
+        }, 
+        {
+            id : "6",
+            Name : "Sony Headphone",
+            price : 2099,
+            image : "./pics/6.webp"
+        },
+        {
+            id : "7",
+            Name : "Sony Buds",
+            price : 1799,
+            image : "./pics/7.jpg"
+        },
+        {
+            id : "8",
+            Name : "Oneplus Buds",
+            price : 1899,
+            image : "./pics/8.jpg"
+        },
+        {
+            id : "9",
+            Name : "JBL Headphone",
+            price : 1999,
+            image : "./pics/9.jpg"
+        },
+    ]
+}
+
+export default data;
---
+++ src/components/front/Routing/Routing.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Products from '../Products/Products'
+import Signup from '../Signup/Signup';
+import Cart from '../Cart/Cart';
+import { Route, Routes } from 'react-router-dom'
+
+const Routing = ({ productItems , cartItems , handleAddProduct , handleRemoveProduct , handleCartClearance}) => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Products productItems={productItems} handleAddProduct={handleAddProduct} />}>
+        </Route>
+        <Route path='/signup'element={<Signup />}></Route>
+        <Route path='/cart' element={<Cart cartItems={cartItems}
+         handleAddProduct={handleAddProduct} 
+         handleRemoveProduct={handleRemoveProduct}
+         handleCartClearance={handleCartClearance}/>}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default Routing;
